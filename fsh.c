36dd1a4c3dac3968059f5bb305a1b32e0f49dfd1
Jonathan Hyry <jon@ttcwenatchee.com>
2014-08-04 01:27:51 -0600

    Remove original licensing; LICENSE file in repo takes its place.
    -jh
    
    	modified:   errorHandler.c
    	modified:   fsh.c
    	modified:   shellOps.c
    	modified:   shellTUI.c
    	modified:   shellTUIHdr.h

--- fsh.c
+++ fsh.c
@@ -1,18 +1,5 @@
 /*
- * This program component is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Author: Jonathan Hyry
+ * Original Author: Jonathan Hyry
  * Date Originally Written: 1/29/2012
  */
 
@@ -28,7 +15,7 @@ int main(int argc, char **argv) {
   // times it has been resized makes this easy.
   timesResized = 0;
 
-	printf("Shellac .0.3 Initializing...\n");
+  printf("Shellac .0.3 Initializing...\n");
 
   // Allocate an array of char pointers for input buffer;
   // Allocate space for shell name and argument string.
